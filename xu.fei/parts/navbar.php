0e6727336353c7500613e86187ad160aff75f67f
徐飞 <xufei@xufeideMacBook-Pro.local>
2020-09-14 16:45:29 -0700

    first week assignment

---
+++ xu.fei/parts/navbar.php
@@ -0,0 +1,25 @@
+
+	<nav>
+		
+			<ul class="nav ">
+
+				<li class="nav1 col-xs-1 col-md-3"><a href="index.php">Home</a></li>
+				<li class="nav2 col-xs-1 col-md-3"><a href="">Work</a></li>
+				<li class="nav3 col-xs-1 col-md-3"><a href="">Photography</a></li>
+				<li class="nav4 col-xs-1 col-md-3"><a href="">Motion</a></li>
+				<li class="nav4 col-xs-1 col-md-3"><a href="">Poetry</a></li>
+				<li class="nav4 col-xs-1 col-md-3"><a href="">About</a></li>
+			
+			</ul>
+	</nav>
+
+<!-- <div class="dropdown">
+  <button class="dropbtn">Work</button>
+  <div class="dropdown-content">
+    <a href="#">Museum Redesign</a>
+    <a href="#">Link 2</a>
+    <a href="#">Link 3</a>
+  </div> -->
+
+</div>
+<div class="clear"></div>
