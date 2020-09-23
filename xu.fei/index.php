be3daf76e9e7daf4c4784d9708714acaa85434e2
徐飞 <xufei@xufeideMacBook-Pro.local>
2020-09-22 17:33:30 -0700

    second week assignment

---
+++ xu.fei/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Fei Asher</title>
+	<meta name="viewport" content="width=device-width">
+	<base href="http://www.asherrfei.com/">
+
+
+	<link rel="stylesheet" href="lib/css/styleguide.css">
+	<link rel="stylesheet" href="lib/css/gridsystem.css">
+	<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+	
+	
+	<!-- font family -->
+
+	<link href="https://fonts.googleapis.com/css?family=Raleway:600&display=swap" rel="stylesheet">
+	
+	<link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
+
+	<link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+
+	<link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+
+	<link href="https://fonts.googleapis.com/css2?family=Gothic+A1:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+</head>
+
+<body>
+<header>
+		
+	<nav>
+		
+			<ul class="nav ">
+
+				<li class="nav1 col-xs-1 col-md-3"><a href="">Home</a></li>
+				<li class="nav2 col-xs-1 col-md-3"><a href="">Work</a></li>
+				<li class="nav3 col-xs-1 col-md-3"><a href="">Photography</a></li>
+				<li class="nav4 col-xs-1 col-md-3"><a href="">Motion</a></li>
+				<li class="nav4 col-xs-1 col-md-3"><a href="">Poetry</a></li>
+				<li class="nav4 col-xs-1 col-md-3"><a href="">About</a></li>
+			
+			</ul>
+	</nav>
+
+<div class="clear"></div>
+		<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+</header>
+
+
+
+	<div class="firstword">
+	<h1>HI! I'M FEI XU_______
+	<br><br>PASSIONATE ABOUT OBSERVING, THINKING, UNDERSTANDING AND TRYING NEW THINGS.</h1>
+	</div>
+
+</body>
+</html>
+</html>
